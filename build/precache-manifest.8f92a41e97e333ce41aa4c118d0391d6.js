b18e7910f7446048c1e1f7a8e7f0277e49684cc5
rohitmandal1 <51334695+rohitmandal1@users.noreply.github.com>
2019-06-24 11:09:05 +0530

    Add files via upload

---
+++ build/precache-manifest.8f92a41e97e333ce41aa4c118d0391d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e00f0ad9f7364995d5f396f52a8a49e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ce6c6edf188820f1be98",
+    "url": "/static/css/main.a71a2758.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "ce6c6edf188820f1be98",
+    "url": "/static/js/main.413b14a7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
